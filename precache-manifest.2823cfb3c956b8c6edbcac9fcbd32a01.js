ede54ba3b6d2ff7086833b55e1a073d969832311
sebastian Chunley <sebastian.chunley@gmail.com>
2022-05-20 13:09:25 -0400

    Updates

---
+++ precache-manifest.2823cfb3c956b8c6edbcac9fcbd32a01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c3ceb18fc6ce9827f8",
+    "url": "/Chuse-React-Portfolio/static/css/main.ef45dfda.chunk.css"
+  },
+  {
+    "revision": "48c3ceb18fc6ce9827f8",
+    "url": "/Chuse-React-Portfolio/static/js/main.48c3ceb1.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Chuse-React-Portfolio/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/Chuse-React-Portfolio/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "c1f66344a0a4e0dedbd0",
+    "url": "/Chuse-React-Portfolio/static/js/runtime~main.c1f66344.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Chuse-React-Portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8699f64be79958b2e86ffd108544e87e",
+    "url": "/Chuse-React-Portfolio/index.html"
+  }
+];
