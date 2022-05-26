26faa77f8c43ea906666441433087a4ce8b2609f
sebastian Chunley <101592116+chuseblok@users.noreply.github.com>
2022-05-26 19:27:59 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PortfolioContainer from "./components/PortfolioContainer";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <div>
+        <div>
+          <PortfolioContainer />;
+        </div>
+        <Footer />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+const Project = () => {
+  return (
+    <h2 class='container'>
+      <div>About Me</div>
+      <p>
+        My name is Sebastian Chunley, and I am striving to be a fullstack
+        developer. I have been trained in MongoDB, Express, React, and Node. I
+        have worked on two cool group projects so far and learned the power
+        teamwork and communication. Project
+      </p>
+      <li>
+        <a href='https://github.com/jdmill/Eventplan'>Eventplan</a>
+        <a href='https://github.com/choilina16/know-your-teacher'>
+          Know Your Teacher
+        </a>
+      </li>
+    </h2>
+  );
+};
+
+export default Project;
